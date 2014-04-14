d7812dc5fcd3a6d5d259c8129c1e54b920333290
Matt Butterfield <mbutterfield@onshift.com>
2014-04-13 22:26:22 -0400

    use jinja2 to generate kml

--- footprinter.py
+++ footprinter.py
@@ -7,34 +7,10 @@ import math
 from pyproj import Proj
 from pyproj import transform
 from optparse import OptionParser
+from jinja2 import Environment, FileSystemLoader
 
 
 class Photo(object):
-    data_fields = ['']
-    photo_id = 0
-    ground_height = 0
-    x_value = 0
-    y_value = 0
-    z_value = 0
-    # will need to convert o,p,k to radians
-    o_value = 0
-    p_value = 0
-    k_value = 0
-    rad_o_Value = 0
-    rad_p_Value = 0
-    rad_k_value = 0
-    corner_coordinates = [[0, 0], [0, 0], [0, 0], [0, 0]]
-    # rotation matrix elements
-    r11 = 0
-    r12 = 0
-    r13 = 0
-    r21 = 0
-    r22 = 0
-    r23 = 0
-    r31 = 0
-    r32 = 0
-    r33 = 0
-    epsg_code = 0
     # UCX image coordinates and focal length
     UCX_IMAGE_Y_FRONT = -33.912
     UCX_IMAGE_Y_BACK = 33.912
@@ -56,6 +32,7 @@ class Photo(object):
         self.rad_o_value = math.radians(self.o_value)
         self.rad_p_value = math.radians(self.p_value)
         self.rad_k_value = math.radians(self.k_value)
+        self.corner_coordinates = [[0, 0], [0, 0], [0, 0], [0, 0]]
         self.compute_rotation_matrix()
 
     def compute_rotation_matrix(self):
@@ -122,99 +99,23 @@ class Photo(object):
             self.corner_coordinates[i][0] = x2
             self.corner_coordinates[i][1] = y2
 
-    def to_kml(self):
-        # returns empty string if grouping separator is detected
-        if self.data_fields[0] != "#":
-            return  "    <Placemark>\n        <name>" + str(self.photo_id) + "</name>\n        <description>" \
-                    + "xValue=" + str(self.x_value) \
-                    + "\nyValue=" + str(self.y_value) \
-                    + "\nzValue=" + str(self.z_value) \
-                    + "\noValue=" + str(self.o_value) \
-                    + "\npValue=" + str(self.p_value) \
-                    + "\nkValue=" + str(self.k_value) \
-                    + "\nGround Height=" + str(self.ground_height) \
-                    + "\nCRS EPSG code=" + str(self.epsg_code) \
-                    + "</description>\n" \
-                    + "        <styleUrl>#msn_ylw-pushpin0</styleUrl>\n" \
-                    + "        <Polygon>\n" \
-                    + "            <tessellate>1</tessellate>\n" \
-                    + "            <outerBoundaryIs>\n" \
-                    + "                <LinearRing>\n" \
-                    + "                    <coordinates>\n" \
-                    + str(self.corner_coordinates[0][0]) + "," + str(self.corner_coordinates[0][1]) + "," + str(self.ground_height) + " " \
-                    + str(self.corner_coordinates[1][0]) + "," + str(self.corner_coordinates[1][1]) + "," + str(self.ground_height) + " " \
-                    + str(self.corner_coordinates[3][0]) + "," + str(self.corner_coordinates[3][1]) + "," + str(self.ground_height) + " " \
-                    + str(self.corner_coordinates[2][0]) + "," + str(self.corner_coordinates[2][1]) + "," + str(self.ground_height) + "\n" \
-                    + "                    </coordinates>\n" \
-                    + "                </LinearRing>\n" \
-                    + "            </outerBoundaryIs>\n" \
-                    + "        </Polygon>\n    </Placemark>\n"
-
 
 def main(file_name, ground_height, epsg_code):
-    output = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" + \
-             "\n<kml xmlns=\"http://www.opengis.net/kml/2.2\"" + \
-             " xmlns:gx=\"http://www.google.com/kml/ext/2.2\"" + \
-             " xmlns:kml=\"http://www.opengis.net/kml/2.2\"" + \
-             " xmlns:atom=\"http://www.w3.org/2005/Atom\">\n" + \
-             "<Document>\n" + \
-             "    <name>FOOTPRINTS</name>\n" + \
-             "    <open>1</open>\n" + \
-             "    <Style id=\"sh_ylw-pushpin\">\n" + \
-             "        <IconStyle>\n" + \
-             "            <scale>1.3</scale>\n" + \
-             "            <Icon>\n"+ \
-             "                <href>http://maps.google.com/mapfiles/kml/pushpin/ylw-pushpin.png</href>\n" + \
-             "            </Icon>\n" + \
-             "            <hotSpot x=\"20\" y=\"2\" xunits=\"pixels\" yunits=\"pixels\"/>\n" + \
-             "        </IconStyle>\n" + \
-             "        <LineStyle>\n" + \
-             "            <color>87000000</color>\n" + \
-             "            <width>1.9</width>\n" + \
-             "        </LineStyle>\n" + \
-             "        <PolyStyle>\n" + \
-             "            <color>7300aa00</color>\n" + \
-             "            <outline>0</outline>\n" + \
-             "        </PolyStyle>\n" + \
-             "    </Style>\n" + \
-             "    <Style id=\"sn_ylw-pushpin\">\n" + \
-             "        <IconStyle>\n" + \
-             "            <scale>1.1</scale>\n" + \
-             "            <Icon>\n" + \
-             "                <href>http://maps.google.com/mapfiles/kml/pushpin/ylw-pushpin.png</href>\n" + \
-             "            </Icon>\n" + \
-             "            <hotSpot x=\"20\" y=\"2\" xunits=\"pixels\" yunits=\"pixels\"/>\n" + \
-             "        </IconStyle>\n" + \
-             "        <LineStyle>\n" + \
-             "            <color>87000000</color>\n" + \
-             "            <width>1.9</width>\n" + \
-             "        </LineStyle>\n" + \
-             "        <PolyStyle>\n" + \
-             "            <color>7300aa00</color>\n" + \
-             "            <outline>0</outline>\n" + \
-             "        </PolyStyle>\n" + \
-             "    </Style>\n" + \
-             "    <StyleMap id=\"msn_ylw-pushpin0\">\n" + \
-             "        <Pair>\n" + \
-             "            <key>normal</key>\n" + \
-             "            <styleUrl>#sn_ylw-pushpin</styleUrl>\n" + \
-             "        </Pair>\n" + \
-             "        <Pair>\n" + \
-             "            <key>highlight</key>\n" + \
-             "            <styleUrl>#sh_ylw-pushpin</styleUrl>\n" + \
-             "        </Pair>\n" + \
-             "    </StyleMap>)\n"
-
     with open(file_name) as f:
         input_lines = f.readlines()
+
+    footprints = []
 
     for line in input_lines:
         p = Photo(line, ground_height, epsg_code)
         p.compute_corner_coordinates()
-        output += p.to_kml()
+        footprints.append(p)
 
-    output += "</Document>\n</kml>"
-    print output
+    env = Environment(loader=FileSystemLoader('.'))
+    template = env.get_template('output_tmpl.kml')
+    t = template.render(footprints=footprints)
+    with open('output.kml', 'wb') as f:
+        f.write(t)
 
 
 if __name__ == '__main__':
